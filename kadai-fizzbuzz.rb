3d549645efccf4623d573a9c54978a8491541811
kunimoto.taishi <taisi.man526@gmail.com>
2022-01-20 07:37:49 +0000

    add git-logo image

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -2,18 +2,18 @@ num_max = 100
 
 def FizzBuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    puts 'FizzBuzz'
+    'FizzBuzz'
   elsif num % 3 == 0
-    puts 'Fizz'
+    'Fizz'
   elsif num % 5 == 0
-    puts 'Buzz'
+    'Buzz'
   else 
-    puts num
+    num
   end
 end
 
 (1..num_max).each do |num|
-  FizzBuzz (num)
+  puts FizzBuzz (num)
 end
 
 
